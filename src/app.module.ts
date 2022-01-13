8604910a36c188ef78eb10c856cdc7207952e21a
oleksandr.khomenko <oleksandr.khomenko@leverx.com>
2022-01-13 15:09:38 +0200

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://alex:1234@alex.dhzz7.mongodb.net/test?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
